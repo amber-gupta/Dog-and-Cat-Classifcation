95d34f943c5e7eb5a76e2400893608edfd02ba2e
amber-gupta <ambergupta516@gmail.com>
2022-07-21 15:56:46 +0530

    runtime added

--- cnn.py
+++ cnn.py
@@ -62,17 +62,17 @@ print("Saved model to disk")
 
 
 
-#
-# import numpy as np
-# from keras.preprocessing import image
-# test_image = image.load_img('cat.11.jpg', target_size = (64, 64))
-# test_image = image.img_to_array(test_image)
-# test_image = np.expand_dims(test_image, axis = 0)
-# result = model.predict(test_image)
-# training_set.class_indices
-# if result[0][0] == 1:
-#     prediction = 'dog'
-#     print(prediction)
-# else:
-#     prediction = 'cat'
-#     print(prediction)
+
+import numpy as np
+from keras.preprocessing import image
+test_image = image.load_img('cat.11.jpg', target_size = (64, 64))
+test_image = image.img_to_array(test_image)
+test_image = np.expand_dims(test_image, axis = 0)
+result = model.predict(test_image)
+training_set.class_indices
+if result[0][0] == 1:
+    prediction = 'dog'
+    print(prediction)
+else:
+    prediction = 'cat'
+    print(prediction)
